0099b74f83c04afe1ee93e54f3314197e055fcca
cristianoBV <cristianovieirati@gmail.com>
2021-03-27 01:02:53 -0300

    Iniciando Aplicacao

---
+++ src/test/java/br/money/borges/moneyapi/MoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.money.borges.moneyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
